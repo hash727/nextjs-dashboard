f2a4663f828ebde1f533d48ee86df92586953aba
hash727 <pharishkumar123@gmail.com>
2025-03-16 16:26:19 +0530

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import "@/app/ui/global.css";
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: {
+    template:"%s | HasH Dashboard - Bellary",
+    default: 'Hash Dashgoard - Bellary'
+  },
+  description: 'The Next.js Course Dashboard, Built with App router, based in Bellary Karnataka, India',
+  metadataBase: new URL('https://nextjs-dashboard-amber-eight-53.vercel.app/')
+}
 
 export default function RootLayout({
   children,
@@ -8,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
